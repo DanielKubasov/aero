06d7b093e5c35e942de3842eb75b055a02a0e51a
Daniel Kubasov <danil.kubasov2007@gmail.com>
2024-11-10 21:58:02 +0500

    feat: authorization ready.

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,6 @@
+import {APP_GUARD} from '@nestjs/core';
 import {Module} from '@nestjs/common';
+import {JwtModule} from '@nestjs/jwt';
 
 import {UsersModule} from '../users/users.module';
 import {PrismaModule} from '../prisma/prisma.module';
@@ -6,9 +8,17 @@ import {PrismaModule} from '../prisma/prisma.module';
 import {AuthService} from './auth.service';
 import {AuthController} from './auth.controller';
 
+import {AuthGuard} from './guards/auth.guard';
+
 @Module({
-    imports: [UsersModule, PrismaModule],
-    providers: [AuthService],
+    imports: [UsersModule, PrismaModule, JwtModule],
+    providers: [
+        AuthService,
+        {
+            provide: APP_GUARD,
+            useClass: AuthGuard,
+        },
+    ],
     controllers: [AuthController],
 })
 export class AuthModule {}
