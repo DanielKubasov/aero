566cd1550aad9385d2091236e9168ade81cdde07
Daniel Kubasov <danil.kubasov2007@gmail.com>
2025-02-22 23:02:10 +0500

    feature: docker dev config ready.

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -2,11 +2,11 @@ module.exports = {
     client: 'postgres',
     connection: {
         name: 'Database connection',
-        host: 'localhost',
-        user: 'postgres',
-        port: 5432,
-        password: 'admin',
-        database: 'aero',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        port: process.env.DB_PORT,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     },
     migrations: {
         directory: './migrations',
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,18 @@ import {ValidationPipe} from '@nestjs/common';
 
 import {AppModule} from './app.module';
 
-const PORT = process.env.PORT || 4000;
+const LOCAL_PORT = process.env.NODE_LOCAL_PORT || 4000;
+const DOCKER_PORT = process.env.NODE_DOCKER_PORT || 4000;
+
+const MODE = process.env.NODE_ENV || 'DEFAULT';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
-    await app.listen(PORT, () => console.log(`The application has been started on port: ${PORT}`));
+    await app.listen(DOCKER_PORT, () => {
+        console.log(`The application has been started in ${MODE} mode.`);
+        console.log(`Local port: ${LOCAL_PORT}, Docker port: ${DOCKER_PORT}`);
+    });
 }
 
 bootstrap();
